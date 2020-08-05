763daeb7fe9aa0bfbd66616b3c28695db0fe7bf1
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2020-08-05 20:09:57 +0300

    working oauth_v2

--- backend/main.py
+++ backend/main.py
@@ -1,56 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 import csv
 import random
 import requests
-import config
+import vk_api
+import os
 
-# vk uses only port 80, redirecting:
-# sudo ncat --sh-exec "ncat 127.0.0.1 80" -l 5000 --keep-open
-#
-#
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 VK_API_ID = 7534914
+
 @app.route("/")
+@app.route('/index')
 def home():
+    """ Render main page if not authorized
+        And redirect to page with swipes if authorized
+    """
+    if 'access_token' not in session:
+        url = '/login'
+        return render_template("index.html", bttnredirect=url)
+    else:
+        return redirect('/swipes')
+
+@app.route("/about")
+def about():
+    return render_template("about.html", user = session['user_id'])
+
+@app.route('/login/')
+def login():
+    """ VK Auth and redirect to /set_cookies with vk code """
+    login_url = vk_api.get_login_url()
+    return redirect(login_url)
+
+@app.route('/set_cookies')
+def set_cookies():
+    """ Get access token and set the cookie with it """
+    global code
+    code = request.args.get('code')
+    access_token, user_id = vk_api.get_access_token(code)
+    session['access_token'] = access_token
+    session['user_id'] = user_id
+
+    res = redirect('/swipes')
+    return res
+
+@app.route('/swipes')
+def swipes():
+
+    access_token = session['access_token']
+    user_id = session['user_id']
+    user = vk_api.get_user_data(access_token, user_id)[0]
+
+    # TODO choose 20 swipe options instead of random 
     # randomly select a movie
     with open('vk_phystech.csv') as f:
         reader = csv.reader(f)
         row = random.choice(list(reader))
 
-    person = {
-        'id': row[1],
-        'name': row[2],
-        'surname': row[3],
-        'sex': row[6],
-        'image': eval(row[8])['photo']['sizes'][-1]['url'],
-   }
-   
-   
-
-    # fetch cover image
-    # call OMDB database
-    #url = f"http://www.omdbapi.com/?t={movie['title']}/&apikey={config.api_key}"
-    # get back the response
-    #response = requests.request("GET", url)
-    # parse result into JSON and look for matching data if available
-    #movie_data = response.json()
-    #if 'Poster' in movie_data:
-    #    movie['image'] = movie_data['Poster']
-    ##if 'imdbRating' in movie_data:
-    #    movie['imdb'] = movie_data['imdbRating']
-    # send all this data to the home.html template
-    
-    return render_template("home.html", person=person)
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
+        person = {
+            'id': row[1],
+            'name': row[2],
+            'surname': row[3],
+            'sex': row[6],
+            'image': eval(row[8])['photo']['sizes'][-1]['url']
+            }    
+    return render_template("home.html", person=person, user = user)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
