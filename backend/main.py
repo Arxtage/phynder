c3c4c4a980b746d78209e17eb96e9faff5e1979c
Arxtage <tsaturyan.aa@phystech.edu>
2020-09-01 16:11:40 +0300

    change swipes func

--- backend/main.py
+++ backend/main.py
@@ -18,8 +18,8 @@ csrf.init_app(app)
 app.secret_key = os.urandom(24)
 
 VK_API_ID = 7534914
-DB_ROOT_DIR = '/Users/izakharkin/Desktop/skoltech/vrarhaptics/deepjest/phynder/backend/'
-# DB_ROOT_DIR = '/Users/inarm/Desktop/PHYNDER.tmp/phynder/backend'
+# DB_ROOT_DIR = '/Users/izakharkin/Desktop/skoltech/vrarhaptics/deepjest/phynder/backend/'
+DB_ROOT_DIR = '/Users/inarm/Desktop/PHYNDER.tmp/phynder/backend'
 PATH_BOYS_CSV = f'{DB_ROOT_DIR}/boys.csv'
 PATH_GIRLS_CSV = f'{DB_ROOT_DIR}/girls.csv'
 PATH_SWIPE_DATA_V2 = f'{DB_ROOT_DIR}/swipe_data_v2.csv'
@@ -69,7 +69,7 @@ def set_cookies():
     session['user_id'] = user_id
     #session['sample'] = sample_partners_v2(user_id)
     #session['partner_counter'] = 0
-    res = redirect('/swipes')
+    res = redirect('/swipes_new')
 
     return res
 
@@ -84,9 +84,9 @@ def sample_partners_v2(user_id):
     if int(user_id) in boys.id.values:
         # проверка какие девочки уже находятся в id_swiped для user_id и семпл из тех, кого там нет
         sample = girls[~girls.id.isin(swipe_data[swipe_data.id == int(user_id)].id_swiped)].sample(1)
+        print("==========LEN OF DB WITH PEOPLE LEFT==========", len(girls[~girls.id.isin(swipe_data[swipe_data.id == int(user_id)].id_swiped)]))
     elif int(user_id) in girls.id.values:
         sample = boys[~boys.id.isin(swipe_data[swipe_data.id == int(user_id)].id_swiped)].sample(1)
-
     return(sample.to_json(orient='records'), swipe_data)
 
 
@@ -105,12 +105,17 @@ def swipes():
     access_token = session['access_token']
 
     user_id = session['user_id']
-    user_info = vk_api.get_user_data(access_token, user_id)[0]  # убрать в серверную часть
-    
-    #num = session['partner_counter']
+    user_info = vk_api.get_user_data(access_token, user_id)[0]
     session['sample'], swipe_data = sample_partners_v2(user_id)
+
+    swipe_type = request.form['swipe_type']
+    swipe_id = request.form['swipe_id']
+
+    swipe_data = swipe_data.append({'id':int(user_id), 'id_swiped':int(swipe_id), 'action':swipe_type}, ignore_index=True)
+    swipe_data.to_csv(PATH_SWIPE_DATA_V2, index=False)
+
     list_of_dicts_of_partners = json.loads(session['sample'])
-    partner = list_of_dicts_of_partners[0]
+    partner = list_of_dicts_of_partners[0] # one partner
     print('==========PARTNER_ID===========',partner['id'])
     person = {
         'id': partner['id'],
@@ -145,9 +150,8 @@ def swipes_new():
 
     user_id = session['user_id']
     user_info = vk_api.get_user_data(access_token, user_id)[0]  # убрать в серверную часть
-
     session['sample'], swipe_data = sample_partners_v2(user_id)
-    
+
     swipe_type = request.form['swipe_type']
     swipe_id = request.form['swipe_id']
 
