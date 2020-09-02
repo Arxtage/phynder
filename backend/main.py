a458aa63f54b3d956d9654b992461aa6c1a24a0e
Arxtage <tsaturyan.aa@phystech.edu>
2020-09-02 16:27:14 +0300

    new db format

--- backend/main.py
+++ backend/main.py
@@ -18,8 +18,8 @@ csrf.init_app(app)
 app.secret_key = os.urandom(24)
 
 VK_API_ID = 7534914
-# DB_ROOT_DIR = '/Users/izakharkin/Desktop/skoltech/vrarhaptics/deepjest/phynder/backend/'
-DB_ROOT_DIR = '/Users/inarm/Desktop/PHYNDER.tmp/phynder/backend'
+# DB_ROOT_DIR = '/Users/izakharkin/Desktop/skoltech/vrarhaptics/deepjest/phynder/backend/static/db'
+DB_ROOT_DIR = '/Users/inarm/Desktop/PHYNDER.tmp/phynder/backend/static/db'
 PATH_BOYS_CSV = f'{DB_ROOT_DIR}/boys.csv'
 PATH_GIRLS_CSV = f'{DB_ROOT_DIR}/girls.csv'
 PATH_SWIPE_DATA_V2 = f'{DB_ROOT_DIR}/swipe_data_v2.csv'
